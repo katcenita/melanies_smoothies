38058f025fadfaa21ae279c6eb35154ab820ec78
katcenita <katrina.c@kollosche.com.au>
2024-09-16 07:57:10 +0800

    Update streamlit_app.py
    insert json and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,12 @@ if ingredients_list :
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
+    
+    import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    #st.text(fruityvice_response).json())
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
   ##  st.write(my_insert_stmt)
   ## st.stop()
     

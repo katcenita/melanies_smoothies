8ce1e2c47f3fc6c1d33c1c33b8d555e8e5b2de82
katcenita <katrina.c@kollosche.com.au>
2024-09-16 08:09:45 +0800

    Update streamlit_app.py
    moved import to 4th line, added json to line 33 on ingredient list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -41,16 +42,15 @@ if ingredients_list :
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
     #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
     
-    import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    #st.text(fruityvice_response).json())
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
     
   ##  st.write(my_insert_stmt)
   ## st.stop()

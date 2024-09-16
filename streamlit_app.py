29098cb4ec8748e6de2a586b4c80dda30692c28e
katcenita <katrina.c@kollosche.com.au>
2024-09-16 09:20:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name')),col('search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients :'
                                  , my_dataframe
